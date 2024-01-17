2ed9d764e50111016154d90b9ddf26d43c3d61ed
elisabichiappini <elisabichiappini@gmail.com>
2024-01-17 22:54:10 +0100

    feat: scaffolding imgs

--- controller/db.php
+++ controller/db.php
@@ -11,25 +11,25 @@ $cani = new Categoria ('cani', '<i class="fa-solid fa-dog"></i>');
 $gatti = new Categoria ('gatti', '<i class="fa-solid fa-cat"></i>');
 
 //istanzio classe Prodotto
-$product1 = new Prodotto (12, 'x', 120, $cani);
+$product1 = new Prodotto (12, 'x', 120, "xx.jpg", $cani);
 
 //istazio classe Cibo
-$cibo_cuccioli = new Cibo (15, 'Trainer Puppy Salmon', 120, $cani);
+$cibo_cuccioli = new Cibo (15, 'Trainer Puppy Salmon', "https://shop-cdn-m.mediazs.com/bilder/letto/cozy/kingdom/4/800/48523_PLA_Cozy_Kuschelbett_Kingdom_FG_11_63_4.jpg", 120, $cani);
 
 //istanzio classe Gioco
-$pallina = new Gioco ( 200,'Palla per dentini', 10, $gatti);
+$pallina = new Gioco ( 200,'Palla per dentini',  "https://shop-cdn-m.mediazs.com/bilder/gioco/per/cani/spiky/ball/large/palla/in/tpr/6/800/62906_PLA_TPR_Spiky_Ball_large_12_FG__53_6.jpg", 10, $gatti);
 
 //istanzio classe Cuccia
-$cuccia_collie = new Cuccia (44, 'Cuccia Giardino Collie', 120, $cani);
+$cuccia_collie = new Cuccia (44, 'Cuccia Giardino Collie', "https://shop-cdn-m.mediazs.com/bilder/letto/basic/1/800/104017_104120_fg_8187_1.jpg", 120, $cani);
 
 
-$prodottiArray = [
+$objsArray = [
     $cibo_cuccioli,
     $pallina,
     $cuccia_collie,
 ];
 
 echo "<pre>";
-var_dump($prodottiArray);
+var_dump($objsArray);
 echo "</pre>";
 ?>
--- view/index.php
+++ view/index.php
@@ -1,6 +1,7 @@
 <?php
 //inclusione con direttiva dir del file Prodotto contenenti le classi
 require_once __DIR__ . '/../controller/db.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -24,38 +25,24 @@ require_once __DIR__ . '/../controller/db.php';
     <!--/Intestazione Pet Shop-->
     <!--Corpo progetto-->
     <main class="container">
+        <!--Contenitore-->
         <div class="row">
-            <div class="col-4">
-                <div class="card width-100">
-                    <img src="https://shop-cdn-m.mediazs.com/bilder/letto/cozy/kingdom/4/800/48523_PLA_Cozy_Kuschelbett_Kingdom_FG_11_63_4.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Buy</a>
+            <?php foreach($objsArray as $obj) :?>
+                <div class="col-4">
+                <!--Card-->
+                    <div class="card width-100">
+                        <img src="<?php echo $obj->get_img()?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Buy</a>
+                        </div>
                     </div>
+                    <!--/Card-->
                 </div>
-            </div>
-            <div class="col-4">
-                <div class="card width-100">
-                    <img src="https://shop-cdn-m.mediazs.com/bilder/letto/cozy/kingdom/4/800/48523_PLA_Cozy_Kuschelbett_Kingdom_FG_11_63_4.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Buy</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="card width-100">
-                    <img src="https://shop-cdn-m.mediazs.com/bilder/letto/cozy/kingdom/4/800/48523_PLA_Cozy_Kuschelbett_Kingdom_FG_11_63_4.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Buy</a>
-                    </div>
-                </div>
-            </div>
+            <?php endforeach ?>
         </div>
+        <!--/Contenitore-->
     </main>
     <!--Corpo progetto-->
 </body>
